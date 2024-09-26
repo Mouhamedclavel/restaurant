218b7957e0b61da94c26c2f926faeab3c5eff001
laspi <pro43071919@gmail.com>
2024-09-26 23:33:42 +0000

    background to all pages

--- resources/views/manager/reports/index.blade.php
+++ resources/views/manager/reports/index.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     @include('partials.dashboard_navbar', ['user' => auth()->user()])
-    <main class="flex-grow">
+    <main class="flex-grow" style="background-image: url('https://s3-alpha-sig.figma.com/img/50f1/de24/70c618c674904eddc922480f3caca474?Expires=1728259200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SOjcfTU3a3qfGdutlhKAT~zdAOrMpDhTaRCLFg1MDD6OKPoF5Jy2ksy~MGsu1yi2qgqdVfBtec-3fRlCmmUcPz6Jr6NyvQpbTLOX6FRdwy~LhJMTKwpnDMh9RkC5HjuNHuYw-BizbU7WHl4LXHEmHrTEDcRuqlpwkn5xrOX7sM9kNHunCFZke-PpVAQFmTBXUAp1xvOJHDAcInUnO8qgI3mP7CHvd~2756098rj5LFstt~cRhIDhAK5yl7bvcnQ3utGlTwUDU6AH6fAPQyDOHgT5p2LPs34Iou~34wN3j~V-hOqJR4FCoxutf-4WemjWclwCbl~I472HuOr2h8A2KA__');  background-size: cover; background-position: center;">
+
         <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
             <form action="{{ route('manager.reports.index') }}" method="GET" class="mb-6">
                 <div class="flex items-center space-x-4">
